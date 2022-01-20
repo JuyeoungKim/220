503b8fcabe81a0f0efa764ff9e50d14595b6eec6
Ben <kimj4@g.cofc.edu>
2022-01-20 02:37:25 -0500

    hw1 complete

--- assignments/hw1/hw1.py
+++ assignments/hw1/hw1.py
@@ -1,35 +1,54 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: <Juyeoung Kim>
+<hw1>.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: <This assignment need basic calculation and input method>
 
 Certification of Authenticity:
 <include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
+I certify that this assignment is my own work, but I discussed it with: <Juyeoung Kim>
 """
 
 
 def calc_rec_area():
-    pass
-
+    length = eval(input("Enter the length: "))
+    width = eval(input("Enter the width: "))
+    area = length * width
+    print("Area =", area)
+calc_rec_area()
 
 def calc_volume():
-    pass
-
+    length = eval(input("Enter the length:"))
+    width = eval(input("Enter the width:"))
+    height = eval(input("Enter the height:"))
+    volume = length * width * height
+    print("Volume =", volume)
+calc_volume()
 
 def shooting_percentage():
-    pass
+    total_shots = eval(input("Enter the player's total shots:"))
+    shots_made = eval(input("How many shots the player made?:"))
+    shoot_per = ((shots_made / total_shots) * 100)
+    print("Shooting percentage is:", shoot_per, "%")
+shooting_percentage()
 
 
 def coffee():
-    pass
+    coffee_amount = eval(input("How many pounds of coffee would you like?:"))
+    coffee_cost = 10.50 * coffee_amount
+    shipping_cost = 0.86 * coffee_amount
+    fixed_cost = 1.5
+    total_cost = coffee_cost + shipping_cost + fixed_cost
+    print("You're total is:", "$", total_cost)
+coffee()
 
 
 def kilometers_to_miles():
-    pass
-
+    kilometers = eval(input("How many kilometers did you travel?:"))
+    miles = kilometers / 1.61
+    print("That's", miles, "miles!")
+kilometers_to_miles()
 
 if __name__ == '__main__':
     pass
